7635a9d9dfec3c0c48ab9b41849a4b14a3d55a6c
Jacob Rosenthal <jakerosenthal@gmail.com>
2014-05-01 20:32:57 -0700

    strange simlinks snuck in

--- inc/ringbuffer.h
+++ inc/ringbuffer.h
@@ -1,1 +1,28 @@
-/Users/jacobrosenthal/Documents/Code/SkynetSpark/src/Ringbuffer.h
+#ifndef _RINGBUFFER_H
+#define _RINGBUFFER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+class ringbuffer {
+	public:
+	    ringbuffer(size_t _size);
+
+	    ~ringbuffer();
+
+	    void push(unsigned char c);
+	    
+	    unsigned char pop();
+
+	    unsigned char peek();
+
+	    bool available();
+
+	private:
+	    unsigned char * buffer;
+	    size_t size;
+	    size_t head;
+	    size_t tail;
+};
+
+#endif // _RINGBUFFER_H
